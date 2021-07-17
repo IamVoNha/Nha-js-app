caff1c100518d4a3f2bb90fbca74c9c171dbc00f
IamVoNha <ov_ahn@yahoo.com>
2021-07-16 22:42:15 -0400

    added pokemonList variable to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+let pokemonList = [
+  {
+    name: 'Balbasaur',
+    height: 0.7,
+    types: ['grass','poison'],
+  },
+  {
+    name: 'Ivysaur',
+    height: 1,
+    types: ['grass','poison'],
+  },
+  {
+    name: 'Venusaur',
+    height: 2,
+    types: ['grass','poison'],
+  },
+  {
+    name: 'Charmander',
+    height: 0.6,
+    types: 'fire',
+  },
+  {
+    name: 'Charmeleon',
+    height: 1.1,
+    types: 'fire',
+  },
+  {
+    name: 'Charizard',
+    height: 1.7,
+    types: ['fire','flying'],
+  },
+  {
+    name: 'Squirtle',
+    height: 0.5,
+    types: 'water',
+  },
+  {
+    name: 'Wartortle',
+    height: 1,
+    types: 'water',
+  },
+  {
+    name: 'Balbasaur',
+    height: 1.6,
+    types: 'water',
+  },
+];
