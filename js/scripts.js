0fba6d021d219d51f60c903047c28215c7e712ec
IamVoNha <ov_ahn@yahoo.com>
2021-07-22 23:23:20 -0400

    created IIFE function to add() & getAll(), and updated foreach function!

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,64 @@
-let pokemonList = [
-  {
-    name: 'Balbasaur',
-    height: 0.7,
-    types: ['grass','poison'],
-  },
-  {
-    name: 'Ivysaur',
-    height: 1,
-    types: ['grass','poison'],
-  },
-  {
-    name: 'Venusaur',
-    height: 2,
-    types: ['grass','poison'],
-  },
-  {
-    name: 'Charmander',
-    height: 0.6,
-    types: ['fire'],
-  },
-  {
-    name: 'Charmeleon',
-    height: 1.1,
-    types: ['fire'],
-  },
-  {
-    name: 'Charizard',
-    height: 1.7,
-    types: ['fire','flying'],
-  },
-  {
-    name: 'Squirtle',
-    height: 0.5,
-    types: ['water'],
-  },
-  {
-    name: 'Wartortle',
-    height: 1,
-    types: ['water'],
-  },
-  {
-    name: 'Blastoise',
-    height: 1.6,
-    types: ['water'],
-  },
+//IIFE function to getAll() & add()
+let pokemonRepository = (function () {
+    let pokemonList = [
+      {
+        name: 'Balbasaur',
+        height: 0.7,
+        types: ['grass','poison'],
+      },
+      {
+        name: 'Ivysaur',
+        height: 1,
+        types: ['grass','poison'],
+      },
+      {
+        name: 'Venusaur',
+        height: 2,
+        types: ['grass','poison'],
+      },
+      {
+        name: 'Charmander',
+        height: 0.6,
+        types: ['fire'],
+      },
+      {
+        name: 'Charmeleon',
+        height: 1.1,
+        types: ['fire'],
+      },
+      {
+        name: 'Charizard',
+        height: 1.7,
+        types: ['fire','flying'],
+      },
+      {
+        name: 'Squirtle',
+        height: 0.5,
+        types: ['water'],
+      },
+      {
+        name: 'Wartortle',
+        height: 1,
+        types: ['water'],
+      },
+      {
+        name: 'Blastoise',
+        height: 1.6,
+        types: ['water'],
+      },
 ];
 
+  return {
+    getAll: function() {
+      return pokemonList;
+    },
+    add: function(item) {
+      pokemonList.push(item);
+    }
+  };
+})();
 //for loop that iterates over each item in pokemonList!
-pokemonList.foreach(function(pokemon) {
+pokemonRepository.getAll().foreach(function(pokemon) {
   document.write(pokemon.name + ' (height: ' + pokemon.height +')');
 }
 
