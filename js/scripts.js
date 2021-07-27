a2849948d6735daf31052333c6c00158f2393839
IamVoNha <ov_ahn@yahoo.com>
2021-07-27 02:33:15 -0400

    added listItem and event listener to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -49,20 +49,38 @@ let pokemonRepository = (function () {
 ];
 
   function add(pokemon) {
-    pokemonList.push(item);
+    pokemonList.push(pokemon);
   };
 
   function getAll() {
         return pokemonList;
     };
 
+  function showDetails (pokemon){
+    console.log (pokemon)
+  }
+
+  function addListItem(pokemon){
+    let pokemonList = document.querySelector (".pokemon-list");
+    let listpokemon = document.createElement ("li");
+    let button = document.createElement ("button");
+    button.innerText = pokemon.name;
+    button.classList.add ("button-class");
+    listpokemon.appendChild (button);
+    pokemonList.appendChild (listpokemon);
+    button.addEventListener('click', function (event) {
+      showDetails (pokemon);
+    });
+  }
+
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem
   };
 })();
 //for loop that iterates over each item in pokemonList!
 
-pokemonRepository.getAll().forEach(function(pokemon) {
-    document.write(pokemon.name + ' (height: ' + pokemon.height + '), (type: ' + pokemon.type + ')' + '<br><br>');
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
 });
